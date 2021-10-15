a36a0de5ce595c2a65f635e5017c4629a12a3961
Ruslan <tagiyevazeandua@gmail.com>
2021-10-15 16:29:47 +0300

    2

---
+++ head.py
@@ -0,0 +1,112 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import os
+import time
+from datetime import datetime
+
+if not os.path.exists("blank"):
+    os.mkdir("blank")
+
+headers = {
+    'Accept': 'text/html, */*; q=0.01',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'
+}
+
+
+def get_job_url(url):
+    """
+    Возвращает все прямые ссылки на вакансию
+    :param url:
+    :return:
+    """
+    job_urls = []
+    req = requests.get(url=url, headers=headers)
+    soup = BeautifulSoup(req.text, 'lxml')
+    h2_list = soup.find_all('h2', class_='')
+    a_list = []
+    for job in h2_list:
+        a_list.append(job.find('a'))
+    for item in a_list:
+        try:
+            job_urls.append('https://www.work.ua' + item.get('href'))
+        except Exception:
+            continue
+    return job_urls
+
+
+def get_max_page(url):
+    """
+    Нет возможности чтобы получить количество страниц с вакансиями напрямую =\
+    :param url: урл странички с запросом
+    :return: количество страниц с вакансиями.
+    """
+    req = requests.get(url=url, headers=headers)
+    soup = BeautifulSoup(req.text, 'lxml')
+    m_p = soup.find('ul', class_='pagination hidden-xs').find_all('a')
+    max_page = 0
+    for item in m_p:
+        if item.text.isnumeric():
+            num = int(item.text)
+            if num > max_page:
+                max_page = num
+    return max_page
+
+
+def search_job(url=f'https://www.work.ua/jobs-it-python/'):
+    """
+    Собирает все странички с вакансиями,
+    и собирает все ссылки на вакансии в один список
+    :return:
+    """
+    # Сбор всех страничек(необязательно)
+    max_page = get_max_page(url)
+    all_job_url = []
+    # Сохраняю все вакансии в один список(можно передавать ссылки сюда напрямую)
+    for page in range(1, max_page+1):
+        print(f'Страница {page} в обработке...')
+        time.sleep(1)
+        all_job_url.extend(get_job_url(f'{url}?page={page}'))
+    return all_job_url
+
+
+def get_information(urls):
+    print('Извлечение данных...')
+    count = 0
+    vacancies_information = []
+    for url in urls:
+        try:
+            req = requests.get(url=url, headers=headers)
+            soup = BeautifulSoup(req.text, 'lxml')
+            id_v = url.split('/')[-2]
+            vacancy = soup.find('h1', id='h1-name').text
+            company = soup.find('span', class_='glyphicon glyphicon-company text-black glyphicon-large').next_sibling.next_sibling.find('b').text
+            city = soup.find('span', class_='glyphicon glyphicon-map-marker text-black glyphicon-large').next_sibling.strip()
+            requirements = soup.find('span', class_='glyphicon glyphicon-tick text-black glyphicon-large').next_sibling.strip().split('.')
+            requirements = list(map(str.strip, requirements))
+            vacancies_information.append(
+                {
+                    'id': id_v,
+                    'vacancy': vacancy,
+                    'company': company,
+                    'city': city,
+                    'requirements': '. '.join(requirements)
+                }
+            )
+            count += 1
+        except Exception:
+            print(f'{url} не обработан.')
+            continue
+    print(f'{count} вакансий было обработано. Запись в файл началась...')
+    return vacancies_information
+
+def main():
+    cur_date = datetime.now().strftime("%d_%m_%Y")
+    with open(fr"blank\vacancies_information_{cur_date}.json", "a", encoding='utf-8') as file:
+        json.dump(get_information(search_job()), file, indent=4, ensure_ascii=False)
+    time.sleep(2)
+    print('Запись закончена...')
+
+
+if __name__ == '__main__':
+    main()
